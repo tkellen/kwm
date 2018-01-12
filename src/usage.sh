e9c9706c23c8288806472c8e4580aceea637049e
Tyler Kellen <tyler@sleekcode.net>
2018-01-12 10:19:39 -0500

    move logic out of template into function

--- src/usage.sh
+++ src/usage.sh
@@ -1,9 +1,13 @@
 . src/lib/template.sh
 
 ##
-# Show main usage screen, duh.
+# Show main usage screen and prompt for installation if needed.
 #
 usage() {
   template usage main
+  if [[ "$(which $SCRIPT_NAME)" != "$SCRIPT_PATH/$SCRIPT_NAME" ]]; then
+    printf "\n"
+    error "$(template usage install)"
+  fi
   exit 0
 }
